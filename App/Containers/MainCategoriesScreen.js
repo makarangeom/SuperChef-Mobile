9b49eb1574dc03b4652c49e78a70976c828c42b9
makara.ngeom <makara.ngeom@bi-kay.com>
2018-12-26 08:59:33 +0700

    Design MainCategory nearly finish

---
+++ App/Containers/MainCategoriesScreen.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+ 
+import { Platform, StyleSheet, View,Image,Text,ImageBackground,ScrollView,FlatList,TouchableOpacity } from 'react-native';
+ 
+export default class MainCategoriesScreen extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            data:[
+                {id:1,name:'Soup',img:'./Images/soup.jpg',img_uri:'http://1.bp.blogspot.com/-QT7_eXH7Ek4/TmcKKNVv-HI/AAAAAAAAGCc/67ipwWUL2z4/s400/300745_10150277303621526_536581525_8438262_1203344248_n.jpg'},
+                {id:2,name:'Fry',img:'./Images/fry.jpg',img_uri:'http://taidynasty.com/wp-content/uploads/2016/08/General-Tsos-Chicken.jpg?w=640'},
+                {id:3,name:'Roast',img:'./Images/roast.jpg',img_uri:'http://eatdrinkpaleo.com.au/wp-content/uploads/2016/04/paleo-duck-pancakes_900-1.jpg'},
+                {id:4,name:'Dessert',img:'./Images/dessert.jpg',img_uri:'http://s-yoolk-images.s3.amazonaws.com/kh/product_catalog/product_images/large/323130?1468418940'},
+                {id:5,name:'Other',img:'./Images/other.jpg',img_uri:'http://s-yoolk-images.s3.amazonaws.com/kh/product_catalog/product_images/large/323130?1468418940'}, 
+            ],
+        }
+    }
+
+    _renderListCategories=({item, key})=>{
+        var img = item.img
+        return(
+            <TouchableOpacity onPress={()=>this.handClickButton(item, key)} style={{width:'100%'}}>
+                <ImageBackground
+                    style={{width:'100%',height:120,marginBottom:10}}
+                    source={{uri:item.img_uri}}
+                    // source={{uri:'http://1.bp.blogspot.com/-QT7_eXH7Ek4/TmcKKNVv-HI/AAAAAAAAGCc/67ipwWUL2z4/s400/300745_10150277303621526_536581525_8438262_1203344248_n.jpg'}}
+                >
+                    <View style={{width: '100%', height: 120,backgroundColor:'rgba(0,0,0,.6)',justifyContent: 'center'}}>
+                        <Text style={{textAlign:'center',color:'#ffffff',fontWeight:'bold'}}>{item.name}</Text>
+                    </View>
+                </ImageBackground>
+            </TouchableOpacity>
+            // <TouchableOpacity onPress={()=>this.handClickButton(item, key)} style={{height:50,backgroundColor:'#747474',alignItems:'center',justifyContent:'center',marginBottom:20}}><Text style={{fontSize:14,color:'#FFFFFF',fontWeight:'bold'}}>{item.name}</Text></TouchableOpacity>
+        )
+    }
+    
+    render() {
+      return (
+    
+            <View style={{flex:1,padding:10}}>
+                <ScrollView>
+                <FlatList
+                    data={this.state.data}
+                    renderItem={this._renderListCategories}
+                    keyExtractor={(item, index) => index}
+                />
+                            {/* <ImageBackground
+                                style={{width:'100%',height:120,marginBottom:10}}
+                                source={require('./Images/soup.jpg')}
+                                // source={{uri:'http://1.bp.blogspot.com/-QT7_eXH7Ek4/TmcKKNVv-HI/AAAAAAAAGCc/67ipwWUL2z4/s400/300745_10150277303621526_536581525_8438262_1203344248_n.jpg'}}
+                            >
+                                <View style={{width: '100%', height: 120,backgroundColor:'rgba(0,0,0,.6)',justifyContent: 'center'}}>
+                                    <Text style={{textAlign:'center',color:'#ffffff',fontWeight:'bold'}}>Soups</Text>
+                                </View>
+                            </ImageBackground>
+                            <ImageBackground
+                                style={{width:'100%',height:120,marginBottom:10}}
+                                source={require('./Images/fry.jpg')} 
+                                // source = {{uri:'http://taidynasty.com/wp-content/uploads/2016/08/General-Tsos-Chicken.jpg?w=640'}}
+                            >
+                                <View style={{width: '100%', height: 120,backgroundColor:'rgba(0,0,0,.6)',justifyContent: 'center'}}>
+                                    <Text style={{textAlign:'center',color:'#ffffff',fontWeight:'bold'}}>Fry</Text>
+                                </View>
+                            </ImageBackground>
+                            <ImageBackground
+                                style={{width:'100%',height:120,marginBottom:10}}
+                                source={require('./Images/roast.jpg')} 
+                                // source = {{uri:'http://eatdrinkpaleo.com.au/wp-content/uploads/2016/04/paleo-duck-pancakes_900-1.jpg'}}
+                            >
+                                <View style={{width: '100%', height: 120,backgroundColor:'rgba(0,0,0,.6)',justifyContent: 'center'}}>
+                                    <Text style={{textAlign:'center',color:'#ffffff',fontWeight:'bold'}}>Roast</Text>
+                                </View>
+                            </ImageBackground>
+                            <ImageBackground
+                                style={{width:'100%',height:120,marginBottom:10}}
+                                source={require('./Images/dessert.jpg')}  
+                                // source = {{uri:'https://www.malis-restaurant.com/phnompenh/wp-content/uploads/2015/12/khmer-dessert-960x365.jpg'}}
+                            >
+                                <View style={{width: '100%', height: 120,backgroundColor:'rgba(0,0,0,.6)',justifyContent: 'center'}}>
+                                    <Text style={{textAlign:'center',color:'#ffffff',fontWeight:'bold'}}>Dessert</Text>
+                                </View>
+                            </ImageBackground>
+                            <ImageBackground
+                                style={{width:'100%',height:120,marginBottom:10}}
+                                source = {{uri:'https://lh4.googleusercontent.com/proxy/IOepd_REYkMHpKWDeZS7LKCc6Ol6fZVzwf4BOa1x_HPpwT5A9AgDEsJXdycI6hPxiE8_u4AArk9opO3FHPufZ8Cf_MaA6VmKfPMjVlVIV5_9F1U=s0-d'}}
+                            >
+                                <View style={{width: '100%', height: 120,backgroundColor:'rgba(0,0,0,.6)',justifyContent: 'center'}}>
+                                    <Text style={{textAlign:'center',color:'#ffffff',fontWeight:'bold'}}>Popular</Text>
+                                </View>
+                            </ImageBackground>
+                            <ImageBackground
+                                style={{width:'100%',height:120,marginBottom:10}}
+                                source={require('./Images/other.jpg')}
+                                // source = {{uri:'http://s-yoolk-images.s3.amazonaws.com/kh/product_catalog/product_images/large/323130?1468418940'}}
+                            >
+                                <View style={{width: '100%', height: 120,backgroundColor:'rgba(0,0,0,.6)',justifyContent: 'center'}}>
+                                    <Text style={{textAlign:'center',color:'#ffffff',fontWeight:'bold'}}>Other</Text>
+                                </View>
+                            </ImageBackground> */}
+                        
+                </ScrollView>
+            </View>
+
+      );
+    }
+   }
--- App/Navigation/NavigationRouter.js
+++ App/Navigation/NavigationRouter.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import { Router, Scene } from 'react-native-router-flux';
 import LaunchScreen from "../Containers/LaunchScreen";
 import HomeScreen from "../Containers/HomeScreen";
+import MainCategoriesScreen from "../Containers/MainCategoriesScreen";
 
 export default class NavigationRouter extends Component{
     constructor(props){
@@ -13,8 +14,9 @@ export default class NavigationRouter extends Component{
         return(
             <Router>
                 <Scene key = "root">
-                    <Scene initial={true} key = "LaunchScreen" component = {LaunchScreen} title = "LaunchScreen" hideNavBar={true}  />
-                    <Scene initial={true} key = "HomeScreen" component = {HomeScreen} title = "HomeScreen" hideNavBar={true}  />
+                    <Scene key = "LaunchScreen" component = {LaunchScreen} title = "LaunchScreen" hideNavBar={true}  />
+                    <Scene key = "HomeScreen" component = {HomeScreen} title = "HomeScreen" hideNavBar={true}  />
+                    <Scene initial={true} key = "MainCategoriesScreen" component = {MainCategoriesScreen} title = "MainCategoriesScreen" hideNavBar={true}  />
                 </Scene>
             </Router>
         );
